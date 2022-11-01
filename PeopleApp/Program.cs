436e2a7e49b65dc99b04fb6817a508a684e666db
Pyremons <Pyremons@outlook.com>
2022-11-01 12:32:00 +0100

    test

--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -5,7 +5,7 @@ using static System.Console;
 Person bob = new();
 WriteLine(bob.ToString());
 bob.Name = "Bob Smith";
-bob.DateOfBirth new DateTime(1965, 12, 22);
+bob.DateOfBirth : new DateTime(1965, 12, 22);
 WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy",
     arg: bob.Name,
     args bob.DateOfBirth);
