00547b7e3fbfb0f6450cbbd9a415f23d6a0f73fd
Pyremons <Pyremons@outlook.com>
2022-11-01 12:26:41 +0100

    commit initial

---
+++ PacktLibrary/Person.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using static System.Console;
+
+namespace Packt.Shared
+{
+    public class Person
+    {
+        public string Name;
+        public DateTime DateOfBirth;
+    }
+}
---
+++ PeopleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Packt.Shared;
+using static System.Console;
+
+
+Person bob = new();
+WriteLine(bob.ToString());
+bob.Name = "Bob Smith";
+bob.DateOfBirth new DateTime(1965, 12, 22);
+WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy",
+    arg: bob.Name,
+    args bob.DateOfBirth);
